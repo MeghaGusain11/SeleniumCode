6d00b82dfb6d2a7fb81368d87bc40132f4ef90e2
DESKTOP-RBNB4BV\Dell <meghagusain1109@gmail.com>
2020-09-25 13:27:11 +0530

    Extent Reporting added

--- src/main/java/com/qa/pages/RegistrationPage.java
+++ src/main/java/com/qa/pages/RegistrationPage.java
@@ -1,14 +1,14 @@
 package com.qa.pages;
 
-import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
+//import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.Assert;
+
 
 import com.qa.baseclass.BaseClass;
 
